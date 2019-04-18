fd66363716a6af9c86f1f57a641d8043de63c4b3
Devon Mitchell <devonamitchell10@gmail.com>
2019-04-18 17:43:42 -0500

    Still working on stretch goals

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $('#redBtn').on('click', whenIclickTheRedButton);
+    $('#blueBtn').on('click', whenIclickTheBlueButton);
+    $('#greenBtn').on('click', whenIclickTheGreenButton);
+    $('#yellowBtn').on('click', whenIclickTheYellowButton);
 });
 
 let redCount = 0;
@@ -8,6 +11,30 @@ function whenIclickTheRedButton() {
     redCount++;
     $('#redCount').text('Red Count: ' + redCount);
     $('#container').append('<div class="box red"></div>');
+}
+
+let blueCount = 0;
+
+function whenIclickTheBlueButton() {
+    blueCount++;
+    $('#blueCount').text('Blue Count: ' + blueCount);
+    $('#container').append('<div class="box blue"></div>');
+}
+
+let greenCount = 0;
+
+function whenIclickTheGreenButton() {
+    greenCount++;
+    $('#greenCount').text('Green Count: ' + greenCount);
+    $('#container').append('<div class="box green"></div>');
+}
+
+let yellowCount= 0;
+
+function whenIclickTheYellowButton() {
+    yellowCount++;
+    $('#yellowCount').text('Yellow Count: ' + yellowCount);
+    $('#container').append('<div class="box yellow"></div>');
 }
 
 console.log('Im running outside the document ready!');
